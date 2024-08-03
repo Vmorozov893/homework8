cfefc7a2f80852601f395eb0ff2cbcad6d1012ec
vmorozov893 <vmorozov893@gmail.com>
2024-08-03 20:32:58 +0300

    1 Задание

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+import java.util.Arrays;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("1 Задание.\n");
+
+        int[] arr = new int[5];
+        arr[0] = 1_234;
+        arr[1] = 3_567;
+        arr[2] = 4_987;
+        arr[3] = 4_003;
+        arr[4] = 4_673;
+        int sum = 0;
+        for (int i : arr){
+            sum += i;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+
+        System.out.println("\n2 Задание\n");
 
     }
 }
